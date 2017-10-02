0aa575d6adf5b05717165ccb7c88cae80363b679
Midas Lambrichts <MidasLamb@users.noreply.github.com>
2017-10-02 19:18:16 +0200

    Update master with design branch (#24)
    * CSS table
    
    * Courses have button as checkbox

--- views/home.php
+++ views/home.php
@@ -30,11 +30,10 @@
             </div>
             <div class="col-md-8 order-md-1">
                 <h2>Select courses</h2>
-                <table>
+                <table class="table table-responsive table-sm">
                     <thead>
                         <tr>
                             <th>Coursename</th>
-                            <th>Add?</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -43,10 +42,11 @@
                         ?>
                             <tr>
                                 <td>
-                                    <?php echo($course["Name"]) ?>
-                                </td>
-                                <td>
-                                    <input type="checkbox" name="courses[]" onclick="toggleGetAttribute('courses[]','<?php echo($course["Id"]) ?>')" value="<?php echo($course["Id"]) ?>">
+                                    <div class="btn-group" data-toggle="buttons">
+                                        <label class="btn btn-outline-dark btn-block" onclick="toggleGetAttribute('courses[]','<?php echo($course["Id"]) ?>')" value="<?php echo($course["Id"]) ?>">
+                                            <input type="checkbox" name="courses[]"> <?php echo($course["Name"]) ?>
+                                        </label>
+                                    </div>
                                 </td>
                             </tr>
                         <?php
@@ -77,5 +77,8 @@
             document.execCommand('copy');
         }
     </script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 </body>
 </html>
