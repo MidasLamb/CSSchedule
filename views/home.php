c8fcda25f89460032d5bd2208bb62d3f12f381fe
Stijn Caerts <stijncaerts@gmail.com>
2017-10-02 14:22:05 +0200

    fix copy and mobile compatibility

--- views/home.php
+++ views/home.php
@@ -7,7 +7,7 @@
     <title>CSSchedule</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
-<body style="padding-top: 5rem">
+<body style="padding-top:5rem">
     <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
         <div class="">
             <a class="navbar-brand" href="#">CSSchedule</a>
@@ -16,7 +16,18 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-4 order-md-2">
+                <h2>Export to ICS</h2>
+                <div class="form-group">
+                    <input id="linkGenerator" class="form-control" type="text" placeholder="https://csschedule.xyz/calendar/?" readonly>
+                    <div id="linkSelector" style="display:none"></div>
+                </div>
+                <div class="form-group">
+                    <button type="button" class="btn btn-outline-primary" onclick="clipboard()">Copy</button>
+                    <a class="btn btn-primary" href="https://csschedule.xyz/calendar?" download id="downloadICS">Download ICS File</a><br>
+                </div>
+            </div>
+            <div class="col-md-8 order-md-1">
                 <h2>Select courses</h2>
                 <table>
                     <thead>
@@ -43,17 +54,6 @@
                     </tbody>
                 </table>
             </div>
-
-            <div class="col-md-4">
-                <h2>Export to ICS</h2>
-                <div class="form-group">
-                    <input id="linkGenerator" class="form-control" type="text" placeholder="https://csschedule.xyz/calendar/?" readonly>
-                </div>
-                <div class="form-group">
-                    <button type="button" class="btn btn-outline-primary" onclick="clipboard()">Copy</button>
-                    <a class="btn btn-primary" href="https://csschedule.xyz/calendar?" download id="downloadICS">Download ICS File</a><br>
-                </div>
-            </div>
         </div>
     </div>
 
@@ -67,11 +67,12 @@
                 linkElement.placeholder += "&amp;"+attributeName+"="+attributeValue;
             }
 
-            document.getElementById("downloadICS").href= linkElement.placeholder;
+            document.getElementById("linkSelector").innerHTML = linkElement.placeholder;
+            document.getElementById("downloadICS").href= document.getElementById("linkSelector").innerText;
         }
 
         function clipboard(){
-            var linkElement = document.getElementById("linkGenerator");
+            var linkElement = document.getElementById("linkSelector");
             window.getSelection().selectAllChildren(linkElement);
             document.execCommand('copy');
             window.getSelection().empty();
