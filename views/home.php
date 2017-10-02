2ea07a3c506acff19e6baf5a1169f8d0856f0e48
Stijn Caerts <stijncaerts@gmail.com>
2017-10-02 17:52:13 +0200

    Courses have button as checkbox

--- views/home.php
+++ views/home.php
@@ -30,11 +30,10 @@
             </div>
             <div class="col-md-8 order-md-1">
                 <h2>Select courses</h2>
-                <table class="table table-responsive table-sm table-hover">
+                <table class="table table-responsive table-sm">
                     <thead>
                         <tr>
                             <th>Coursename</th>
-                            <th>Add?</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -43,13 +42,11 @@
                         ?>
                             <tr>
                                 <td>
-                                    <?php echo($course["Name"]) ?>
-                                </td>
-                                <td>
-                                    <label class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" name="courses[]" onclick="toggleGetAttribute('courses[]','<?php echo($course["Id"]) ?>')" value="<?php echo($course["Id"]) ?>">
-                                        <span class="custom-control-indicator"></span>
-                                    </label>
+                                    <div class="btn-group" data-toggle="buttons">
+                                        <label class="btn btn-outline-dark btn-block" onclick="toggleGetAttribute('courses[]','<?php echo($course["Id"]) ?>')" value="<?php echo($course["Id"]) ?>">
+                                            <input type="checkbox" name="courses[]"> <?php echo($course["Name"]) ?>
+                                        </label>
+                                    </div>
                                 </td>
                             </tr>
                         <?php
@@ -80,5 +77,8 @@
             document.execCommand('copy');
         }
     </script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 </body>
 </html>
