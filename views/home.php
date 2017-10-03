2e68bfaadc5531d4107c9710290225dada5c73ab
Midas Lambrichts <midas_lambrichts@hotmail.com>
2017-10-03 17:24:41 +0200

    Fix #27

--- views/home.php
+++ views/home.php
@@ -32,12 +32,26 @@
             <div class="col-md-8 order-md-1">
                 <h2>Select courses</h2>
                 <div class="form-group">
-                    <input id="searchText" class="form-control" type="text" placeholder="Search" oninput="filter(this.value)">
+                    <input id="searchText" class="form-control" type="text" placeholder="Search" oninput="filter()">
                 </div>
                 <table class="table table-sm" style="width:100%">
                     <thead>
                         <tr>
-                            <th style="font-size:1.3em;">Coursename</th>
+                            <th style="font-size:1.3em;">
+                            <div class="row">
+                                <div class="col-md-6">
+                                Coursename
+                                </div>
+                                <div class="col-md-6 float-right">
+                                    <div class="btn-group coursename-btn float-right" data-toggle="buttons">
+                                        <label id="showSelectedSwitch" class="btn btn-outline-dark btn-block" onclick="showSelectedToggle()">
+                                            <input type="checkbox"> 
+                                            Show selected Courses only.
+                                        </label>
+                                    </div>  
+                                </div>
+                            </div>
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
@@ -113,24 +127,47 @@
 
         var lastSearchString = "";
 
-        function filter(searchText){
-            searchText = searchText.toLowerCase();
+        function filter(){
+            var searchText = $("#searchText").val().toLowerCase();
             var group;
             if (searchText.length > lastSearchString.length){
-                group = $("table tbody tr:visible");
+                group = $("table tbody tr");
             } else {
                 group = $("table tbody tr");
             }
             group.each(function(){
                 if ($(this).find(".courseName").text().toLowerCase().includes(searchText)){
-                    $(this).show();
+                    $(this).removeClass("hiddenBySearch");
                 } else {
-                    $(this).hide();
+                    $(this).addClass("hiddenBySearch");
                 }
             });
-
             lastSearchString = searchText;
         }
+
+        var showSelected = false;
+
+        function showSelectedToggle(){
+            showSelected = !showSelected;
+            showSelectedCourses();
+        }
+
+        function showSelectedCourses(){
+            if (showSelected){
+                $("table tbody tr").each(function(){
+                    if ($(this).html().includes("active")){
+                        $(this).removeClass("hiddenByShowSelectedCourses");
+                    } else {
+                        $(this).addClass("hiddenByShowSelectedCourses");
+                    }
+                });
+            } else {
+                $("table tbody tr").each(function(){
+                    $(this).removeClass("hiddenByShowSelectedCourses");
+                });
+            }
+        }
+
     </script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
