1332b46d901a54f6707c151082dcb6707875f1cc
Midas Lambrichts <midas_lambrichts@hotmail.com>
2017-10-03 16:57:28 +0200

    Fix #30

--- views/home.php
+++ views/home.php
@@ -88,10 +88,11 @@
 
             document.getElementById("downloadICS").href= linkElement.placeholder;
 
-            window.setTimeout(function(){
-                $('#searchText').get(0).focus();
-            }, 100);
-
+            if ($("#searchText").isOnScreen()){
+                window.setTimeout(function(){
+                    $('#searchText').get(0).focus();
+                }, 100);
+            }
         }
 
         function toggleGetAttribute(attributeName, attributeValue){
@@ -134,5 +135,27 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+    <script>
+        $.fn.isOnScreen = function(){
+	
+            var win = $(window);
+            
+            var viewport = {
+                top : win.scrollTop(),
+                left : win.scrollLeft()
+            };
+            viewport.right = viewport.left + win.width();
+            viewport.bottom = viewport.top + win.height();
+            
+            var bounds = this.offset();
+            bounds.right = bounds.left + this.outerWidth();
+            bounds.bottom = bounds.top + this.outerHeight();
+            
+            return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+            
+        };
+
+    </script>
 </body>
 </html>
